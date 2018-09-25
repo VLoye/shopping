70c2cda3a6d1669eb5affffe8640f94d03f2c6f4
VLoye <974985067@qq.com>
2018-09-25 17:22:40 +0800

    关注收藏

--- src/main/Java/com/team6/controller/FollowController.java
+++ src/main/Java/com/team6/controller/FollowController.java
@@ -6,12 +6,16 @@ import com.team6.service.FollowService;
 import com.team6.service.JedisAdapter;
 import com.team6.service.login.LoginService;
 import com.team6.util.HostHolder;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.noggit.JSONUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -25,6 +29,8 @@ import java.util.Map;
  * @Version 1.0
  * 关注收藏
  **/
+@Api(value = "/follow",tags = "follow",description = "收藏")
+@Controller
 public class FollowController {
     private static final Logger logger = LoggerFactory.getLogger(FollowController.class);
 
@@ -38,21 +44,24 @@ public class FollowController {
     FollowService followService;
 
 
-    @RequestMapping("follow/{id}/{type}")
+    @ApiOperation(value = "关注商品/商家",httpMethod = "Post",notes = "关注商品/商家")
+    @RequestMapping(value = "follow/{id}/{type}" ,method = RequestMethod.POST)
     @ResponseBody
     public String follow(@PathVariable("id")int id,@PathVariable("type")int type){
         Map map = followService.follow(id,type);
         return JSONUtil.toJSON(map);
     }
 
-    @RequestMapping("unfollow/{id}/{type}")
+    @ApiOperation(value = "取关",httpMethod = "Post")
+    @RequestMapping(value = "unfollow/{id}/{type}" ,method = RequestMethod.POST)
     @ResponseBody
     public String unfollow(@PathVariable("id")int id,@PathVariable("type")int type){
         Map map = followService.unfollow(id,type);
         return JSONUtil.toJSON(map);
     }
 
-    @RequestMapping("follows/{type}")
+    @ApiOperation(value = "返回所有关注",httpMethod = "Get")
+    @RequestMapping(value = "follows/{type}",method = RequestMethod.GET)
     @ResponseBody
     public String follows(@PathVariable("type")int type){
         List list = followService.follows(type);
