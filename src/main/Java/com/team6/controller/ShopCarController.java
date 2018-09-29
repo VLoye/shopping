2fa7b2ced33115e0134a70ade0adac94e94f97fe
happynewyear <1002463796@qq.com>
2018-09-29 16:12:57 +0800

    Merge branch 'master' of https://github.com/Puniam/shopping
    # Conflicts:
    #	src/main/Java/com/team6/controller/IndexContorller.java
    #	src/main/Java/com/team6/service/login/LoginServiceImp.java

--- src/main/Java/com/team6/controller/ShopCarController.java
+++ src/main/Java/com/team6/controller/ShopCarController.java
@@ -2,20 +2,15 @@ package com.team6.controller;
 
 import com.team6.service.login.LoginService;
 import com.team6.service.shopcar.ShopCarService;
-import org.apache.commons.math3.analysis.solvers.NewtonRaphsonSolver;
-import org.apache.ibatis.annotations.Param;
 import org.noggit.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import springfox.documentation.spring.web.json.Json;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
