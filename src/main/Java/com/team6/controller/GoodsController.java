71fbc2d129dd879b441dd47299578cb7318494e5
zhengwk <597931083@qq.com>
2018-10-14 16:34:41 +0800

    Merge branch 'master' of https://github.com/597931083/shopping
    # Conflicts:
    #	src/main/webapp/WEB-INF/html/Personality/Seller/SellerGoodManage.html
    #	src/main/webapp/WEB-INF/html/Personality/Seller/SellerProaddSupply.html
    #	src/main/webapp/WEB-INF/html/Personality/Seller/SellerSelledGoods.html
    #	src/main/webapp/WEB-INF/html/Personality/Seller/SellerSendMessage.html
    #	src/main/webapp/WEB-INF/html/Personality/Seller/SellerSendMessageChild.html

--- src/main/Java/com/team6/controller/GoodsController.java
+++ src/main/Java/com/team6/controller/GoodsController.java
@@ -3,11 +3,8 @@ package com.team6.controller;
 import com.team6.entity.Goods;
 import com.team6.service.Goods.GoodsService;
 import com.team6.util.enums.GoodsEnum;
-import org.apache.ibatis.annotations.Param;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +17,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
